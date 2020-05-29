8bc347a2b1df0515b8189a67223acf04505d9d7e
alshoja <alshoja@gmail.com>
2020-05-29 21:22:15 +0530

    reservation

---
+++ process.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Bus Ticketing System</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+	<div class="header">
+		<h1>Ticket Reservation</h1>
+	</div>
+	<form action="" class="register">
+		<h1>BUS Ticket Reservation</h1>
+		<?php if (isset($_POST) == true && empty($_POST) == false) :
+			$chkbox = $_POST['chk'];
+			$bus = $_POST['bus'];
+			$day = $_POST['day'];
+			$month = $_POST['month'];
+			$mob = $_POST['mob'];
+			$type = $_POST['type'];
+			$from = $_POST['from'];
+			$to = $_POST['to'];
+			$root = $_POST['root'];
+			$BX_NAME = $_POST['BX_NAME'];
+			$BX_age = $_POST['BX_age'];
+			$BX_gender = $_POST['BX_gender'];
+			$BX_birth = $_POST['BX_birth'];
+		?>
+			<fieldset class="row1">
+				<legend>Travel Information</legend>
+				<p>
+					<label>BUS Name
+					</label>
+					<input name="bus" type="text" readonly="readonly" value="<?php echo $bus ?>" />
+					<label>Date of journey
+					</label>
+					<input type="text" readonly="readonly" class="small" value="<?php echo $day ?>" />
+					<input type="text" readonly="readonly" class="small" value="<?php echo $month ?>" />
+					<input type="text" readonly="readonly" class="small" value="2020" />
+					<label>Bus Type
+					</label>
+					<input type="text" readonly="readonly" value="<?php echo $type ?>" />
+
+				</p>
+				<p>
+					<label>Boarding From
+					</label>
+					<input name="from" type="text" readonly="readonly" value="<?php echo $from ?>" />
+					<label>To
+					</label>
+					<input name="to" type="text" readonly="readonly" value="<?php echo $to ?>" />
+					<label>Via (Root)
+					</label>
+					<input type="text" readonly="readonly" value="<?php echo $root ?>" />
+
+				</p>
+				<p>
+					<label>Mobile
+					</label>
+					<input name="mob" type="text" readonly="readonly" value="<?php echo $mob ?>" />
+				</p>
+
+				<div class="clear"></div>
+			</fieldset>
+			<fieldset class="row2">
+				<legend>Passenger Details
+				</legend>
+				<table id="dataTable" class="form" border="1">
+					<tbody>
+						<tr>
+							<th></th>
+							<th>Name</th>
+							<th>Age</th>
+							<th>Gender</th>
+							<th>Bearth Pre</th>
+						</tr>
+						<?php foreach ($BX_NAME as $a => $b) { ?>
+							<tr>
+								<td>
+									<?php echo $a + 1; ?>
+								</td>
+								<td>
+									<?php echo $BX_NAME[$a]; ?>
+								</td>
+								<td>
+
+									<?php echo $BX_age[$a]; ?>
+								</td>
+								<td>
+									<?php echo $BX_gender[$a]; ?>
+								</td>
+								<td>
+									<?php echo $BX_birth[$a]; ?>
+								</td>
+							</tr>
+			<?php } ?>
+			</tbody>
+			</table>
+			<div class="clear"></div>
+			</fieldset>
+			<fieldset class="row5">
+				<legend>Terms and Condition</legend>
+				<p>
+					<input class="submit" type="button" value="Make Payment &raquo;" />
+					<a class="submit" href="index.html" type="button"> Back Home <a />
+				</p>
+				<div class="clear"></div>
+			</fieldset>
+		<?php else : ?>
+			<fieldset class="row1">
+				<legend>Sorry</legend>
+				<p>OOps! something went wrong please try again.</p>
+			</fieldset>
+		<?php endif; ?>
+		<div class="clear"></div>
+	</form>
+	<div class="footer">
+		<p>The identification details are required during journey. One of the passenger booked on the ticket should
+			have any of the identity cards ( Passport / PAN Card / Driving License / Photo ID card issued by Central
+			/ State Govt / Student Identity Card with photograph) during the journey in original </p>
+		<p>© 2020 Passenger Tim, all rights reserved</p>
+	</div>
+</body>
+
+</html>
