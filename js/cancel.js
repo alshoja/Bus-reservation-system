266b7addb0a3f16cd69dc1edb389f864f9c5fb0b
alshoja <alshoja@gmail.com>
2020-06-09 00:09:48 +0530

    js updated

--- js/cancel.js
+++ js/cancel.js
@@ -1,10 +1,9 @@
 function cancel() {
-    const txt;
-    const r = confirm("Are you sure you want to cancel");
+    var txt;
+    var r = confirm("Are you sure you want to cancel");
     if (r == true) {
         window.location.href='index.html'
     } else {
         return false
     }
-    doc
 }
